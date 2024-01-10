06b517223acaf4f7e31e75eebeb763e24fae18cb
Saksham Negi <saksham@Sakshams-MacBook-Air.local>
2024-01-10 22:31:28 +0530

    complete

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from flask import Flask,request, render_template
 import numpy as np
+import pandas as pd
 import pickle
 import sklearn
 print(sklearn.__version__)
 #loading models
 dtr = pickle.load(open('dtr.pkl','rb'))
 preprocessor = pickle.load(open('preprocessor.pkl','rb'))
+df = pd.read_csv("./yield_df.csv")
+countries =  df['Area'].unique()
+crops = df['Item'].unique()
 
 #flask app
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', countries = countries, crops=crops, prediction=False)
+
 
 @app.route("/predict",methods=['POST'])
 def predict():
@@ -28,7 +33,7 @@ def predict():
         transformed_features = preprocessor.transform(features)
         prediction = dtr.predict(transformed_features).reshape(1,-1)
 
-        return render_template('prediction.html',prediction = prediction)
+        return render_template('index.html',prediction = prediction, countries=countries, crops=crops)
 
 if __name__=="__main__":
     app.run(debug=True)
